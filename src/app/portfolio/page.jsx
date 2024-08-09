9de86434d52354e9b7afc1fb05cd44546bd754b1
muzzamilhassan <129590453+muzzamilhassan@users.noreply.github.com>
2024-08-09 20:22:31 +0500

    update

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -15,7 +15,7 @@ const Portfolio = () => {
           <div className="hoverr">
           <div>
             <h5></h5>
-            <Link href={'https://headphone-store-muzzi.netlify.app/'} className='text-white hover:text-white' target='_blank'><FaLink/>Headphone-Store</Link>
+            <Link href={'https://headphone-store-muzzi.netlify.app/'} className='text-xl text-white hover:text-white flex' target='_blank'><FaLink className='text-2xl'/>Headphone-Store</Link>
           </div>
 
           </div>
